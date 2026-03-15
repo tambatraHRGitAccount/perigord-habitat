7df3d070dc7ab91ce08a82b196da2492c6d7fb09
tambatraHRGitAccount <randrianasolotambatrahasiniavo@gmail.com>
2026-03-15 15:15:21 +0300

    preprod

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       <body className={`${manrope.className}`}>
         <ThemeProvider
           attribute='class'
-          defaultTheme='system'
+          defaultTheme='light'
           enableSystem
           disableTransitionOnChange>
           {children}
